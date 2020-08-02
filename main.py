cf761b2a2252231f51b0cb5cca264dacf6b56225
Alexander King <alexanderddking@gmail.com>
2020-08-02 23:23:01 +0900

    working but loops

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 import sys
 from PySide2 import QtCore, QtGui, QtWidgets
+from inputs import devices, get_mouse, get_gamepad
 
-app = QtWidgets.QApplication(sys.argv)
-app.setWindowIcon(QtGui.QIcon("emoji/normal.png"))
+def gamepadEvents():
+    """Just print out some event infomation when the gamepad is used."""
+    while 1:
+        events = get_gamepad()
+        for event in events:
+            print(event.code, event.state)
+            mainUI.show()
+            mainUI.move(event.state, 680)
+            mainWidget.exec_()
 
+
+# Make the Window
+mainWidget = QtWidgets.QApplication(sys.argv)
+# Set the Window
+mainWidget.setWindowIcon(QtGui.QIcon("emoji/normal.png"))
+
+# Get Emoji and scale the emoji
+emoji = QtGui.QPixmap('emoji/angry.png')
+smallEmoji = emoji.scaled(240, 240, QtCore.Qt.KeepAspectRatio)
+
+# Setup main UI
+mainUI = QtWidgets.QLabel()
+mainUI.setPixmap(smallEmoji)
+
+# Make the window borderless
+mainUI.setAttribute(QtCore.Qt.WA_NoSystemBackground, True)
+mainUI.setAttribute(QtCore.Qt.WA_TranslucentBackground, True)
 flags = QtCore.Qt.WindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
-# window.setWindowFlags(flags)
+mainUI.setWindowFlags(flags)
 
-pixmap = QtGui.QPixmap('emoji/angry.png')
-small = pixmap.scaled(240, 240, QtCore.Qt.KeepAspectRatio)
-label = QtWidgets.QLabel()
-label.setPixmap(small)
-label.setWindowFlags(flags)
-label.setAttribute(QtCore.Qt.WA_NoSystemBackground, True)
-label.setAttribute(QtCore.Qt.WA_TranslucentBackground, True)
-label.setFixedSize(240, 240)
-label.move(1150, 680)
+# Size and Position of the window
+mainUI.setFixedSize(240, 240)
 
-label.show()
-app.exec_()
+gamepadEvents()
